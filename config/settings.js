7685ee5f9110928f237a4f65fac4ee6739865c65
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2019-02-17 14:02:47 -0500

    added settings

---
+++ config/settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'secret': 'bookXCHANGEsecret',
+  'accessKeyId': 'AKIAJ6PZND3SWGKVTAMQ',
+  'secretAccessKey': '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',
+  'region': 'us-east-1'
+};
