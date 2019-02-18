39bcbabdf513c8fb778ca0283878ef2cc8cc44c5
RJT2nd <robert.jeffrey.thomas@gmail.com>
2019-02-17 20:16:59 -0500

    Delete settings.js

--- config/settings.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'secret': 'bookXCHANGEsecret',
-  'accessKeyId': 'AKIAJ6PZND3SWGKVTAMQ',
-  'secretAccessKey': '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',
-  'region': 'us-east-1'
-};
